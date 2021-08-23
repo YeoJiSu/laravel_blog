6d4e4ed20c0194a9c041f43a6a9d3eeab78d662e
YeoJiSu <duwltn1301@naver.com>
2021-08-23 19:02:02 +0900

    migrate 오류남 ....

--- database/migrations/2014_12_12_200000_create_goods_table.php
+++ database/migrations/2014_12_12_200000_create_goods_table.php
@@ -14,18 +14,21 @@ class CreateGoodsTable extends Migration
     public function up()
     {
         Schema::table('goods', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->Boolean('isGlass');// 안경이나 선글라스냐 
-            $table->string('name');
-            $table->string('price');
+            $table->foreign('owner_id')->references('id')->on('owner');
+            //$table->bigincrements('id');
+            $table->id();
+            $table->Boolean('isGlass')->default(0);// 안경이나 선글라스냐 
+            $table->string('name')->comment("상품명");
+            $table->string('price')->comment("가격");
             $table->string('img_url');
-            $table->Boolean('isCustom');
-            $table->Boolean('isNew');
-            $table->Boolean('isBest');
-            $table->Boolean('isSoldOut');
+            $table->Boolean('isCustom')->default(0);
+            $table->Boolean('isNew')->default(0);
+            $table->Boolean('isBest')->default(0);
+            $table->Boolean('isSoldOut')->default(0);
             $table->foreign('like_id')->references('id')->on('likes');
             $table->foreign('bag_id')->references('id')->on('bag');
             $table->foreign('buy_id')->references('id')->on('buy');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
