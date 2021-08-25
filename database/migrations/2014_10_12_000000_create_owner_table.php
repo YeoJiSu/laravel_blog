d48008db06f23f93f1d5b238c7815ba2f0ab46b8
YeoJiSu <duwltn1301@naver.com>
2021-08-25 18:14:31 +0900

    owner table 수정

--- database/migrations/2014_10_12_000000_create_owner_table.php
+++ database/migrations/2014_10_12_000000_create_owner_table.php
@@ -20,8 +20,6 @@ class CreateOwnerTable extends Migration
             $table->string('password')->comment("비밀번호");;
             $table->string('email')->unique()->comment("이메일");
             $table->string('tel')->unique()->comment("연락처");
-            $table->boolean('private')->default(0)->comment("개인정보 확인 유무");
-            $table->boolean('third_party')->default(0)->comment("제3자 제공 유무");//근데 관리자 테이블에 이게 필요할까..?
             $table->rememberToken();
             $table->timestamps();
         });
