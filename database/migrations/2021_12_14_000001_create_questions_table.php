ca3b8f26fc90940f7bd7447d2e2f8335026ec495
Kyungseo-Park <troublesome.dev@gmail.com>
2021-08-26 02:25:50 +0900

    오너는 필수값이 아니라 nullable로 컬럼을 업데이트함

--- database/migrations/2021_12_14_000001_create_questions_table.php
+++ database/migrations/2021_12_14_000001_create_questions_table.php
@@ -17,10 +17,13 @@ class CreateQuestionsTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            // nullable이 가능한 필드에 nullable() 추가한다.
+            // nullable 이란? 데이터가 생성될 때 필요 없는(필수값) 칼럼을 말한다.
+            // ex) $table->unsignedBigInteger('owner_id')->nullable();
             $table->unsignedBigInteger('owner_id');
             $table->foreign('owner_id')->references('id')->on('owner');
             $table->boolean('is_public')->default(0)->comment('공개유무');
-            $table->string('type')->comment('타입'); //배송, 교환/반품 , 기타 
+            $table->string('type')->comment('타입'); //배송, 교환/반품 , 기타
             $table->string('title')->comment('제목');
             $table->string('content')->comment('내용');
             $table->string('answer')->comment('답변');
